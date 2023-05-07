3a3ac4e28b9db62963bb343fe53d69b83b6a40b4
linhnv <nvlk12haui@gmail.com>
2023-05-07 20:47:55 +0700

    init

---
+++ src/main/java/com/linhnv/efkstack/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.linhnv.efkstack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/linhnv/efkstack/EfkStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhnv.efkstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfkStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
